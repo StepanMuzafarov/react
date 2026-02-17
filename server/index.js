6c2ae755cdb4b31126f0003c78dc907d3b97b606
stepan <stepan.muzafarov@gmail.com>
2026-02-17 22:56:48 +0300

    change work with offers, add data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
 import sequelize from './config/database.js';
 
 import { User } from './models/user.js';
@@ -16,6 +19,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Сервер rental-service работает!' });
 });
@@ -26,12 +34,14 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Маршрут не найден' });
 });
 
+app.use(errorMiddleware)
+
 const start = async () => {
   try {
     await sequelize.authenticate();
     console.log('✅ Подключение к БД установлено успешно!');
     
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
     console.log('✅ Таблицы синхронизированы с базой данных');
     
     app.listen(PORT, () => {
